68964596ff578ced52a2724f3b294bb02a116fb8
codesweaver <codetrain999@gmail.com>
2021-07-12 01:18:38 +0900

    뭔가 찜찜한 트리

---
+++ src/study/TreeApp.java
@@ -0,0 +1,94 @@
+package study;
+
+class Node {
+    private final int data;
+    private Node left;
+    private Node right;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return this.data;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+}
+
+public class TreeApp {
+    public static void main(String[] args) {
+        Node root = new Node(50);
+        add(root, 10);
+        add(root, 53);
+        add(root, 25);
+        add(root, 33);
+        add(root, 75);
+
+        System.out.println(get(root, 33));
+        delete(root, 33);
+        System.out.println(get(root, 33));
+        add(root, 33);
+        System.out.println(get(root, 33));
+    }
+
+    public static Node add(Node node, int data) {
+        if (node == null) {
+            return new Node(data);
+        }
+
+        if (data < node.getData()) {
+            node.setLeft(add(node.getLeft(), data));
+        } else {
+            node.setRight(add(node.getRight(), data));
+        }
+
+        return node;
+    }
+
+    public static Node get(Node node, int data) {
+        if (node == null) {
+            return null;
+        }
+
+        if (node.getData() == data) {
+            return node;
+        }
+
+        if (data < node.getData()) {
+            return get(node.getLeft(), data);
+        }
+        return get(node.getRight(), data);
+    }
+
+    public static Node delete(Node node, int data) {
+        if (node == null) {
+            return null;
+        }
+
+        if (node.getData() == data) {
+            return null;
+        }
+
+        if (data < node.getData()) {
+            node.setLeft(delete(node.getLeft(), data));
+        } else {
+            node.setRight(delete(node.getRight(), data));
+        }
+        return null;
+    }
+}
