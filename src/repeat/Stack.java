dfeb63afab7b47c06acf5fb0513a2a6fe2f0f15a
codesweaver <codetrain999@gmail.com>
2021-07-13 15:21:20 +0900

    임시 저장

---
+++ src/repeat/Stack.java
@@ -0,0 +1,16 @@
+//package repeat;
+//
+//class Stack {
+//    class Node<T> {
+//        private T data;
+//        private Node<T> next;
+//
+////        public Node
+//    }
+//}
+//
+//public class Stack {
+//    public static void main(String[] args) {
+//
+//    }
+//}
