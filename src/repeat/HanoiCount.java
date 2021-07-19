ab5502a409fe7500a8ee9a0994e475e5a9b2a084
codesweaver <codetrain999@gmail.com>
2021-07-19 18:15:52 +0900

    하노이 갯수만 구하기

--- src/repeat/HanoiCount.java
+++ src/repeat/HanoiCount.java
@@ -2,10 +2,19 @@ package repeat;
 
 public class HanoiCount {
     public static void main(String[] args) {
+        System.out.println(hanoi(1));
+        System.out.println(hanoi(2));
         System.out.println(hanoi(3));
+        System.out.println(hanoi(4));
+        System.out.println(hanoi(5));
+        System.out.println(hanoi(6));
+        System.out.println(hanoi(7));
     }
 
     private static int hanoi(int number) {
-//        return 0;
+        if (number <= 1) {
+            return number;
+        }
+        return (hanoi(number-1) * 3) - (hanoi(number-2) * 2);
     }
 }
